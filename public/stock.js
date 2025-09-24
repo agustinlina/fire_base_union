69f504cc191e65eee2cacf88cb69cf544f02f652
agustin linares <agustinlinares2009@hotmail.com>
2025-09-24 07:05:45 -0300

    hola

--- public/stock.js
+++ public/stock.js
@@ -3,11 +3,7 @@
 // ====== Dólar manual (override) ======
 // Si querés fijar el dólar manualmente, poné acá un número (e.g., 950).
 // Si es 0, toma el valor desde la API (Oficial) y se actualiza a las 19:01 AR.
-<<<<<<< HEAD
-let DOLAR_TOTAL = 1385
-=======
-let DOLAR_TOTAL = 1430
->>>>>>> ca9ba161a2aafd8ccb63fe6207081fc251c5c217
+let DOLAR_TOTAL = 1470
 
 const ENDPOINTS = {
   olavarria:
